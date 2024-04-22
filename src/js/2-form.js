fba51f9f8a90482409d26b010cad6b809a8eeced
Kholodaet <kholodaet@icloud.com>
2024-04-22 20:21:21 +0300

    update

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,7 +2,6 @@ const form = document.querySelector('.feedback-form');
 const email = form.elements.email;
 const message = form.elements.message;
 
-// Функція для збереження даних у локальне сховище
 function saveFormData() {
   const formData = {
     email: email.value.trim(),
@@ -11,24 +10,19 @@ function saveFormData() {
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
-// Відслідковуємо подію введення даних у поля форми
 form.addEventListener('input', function () {
   saveFormData();
 });
 
-// Перевіряємо форму при її відправці
 form.addEventListener('submit', function (event) {
   event.preventDefault();
 
   const emailValue = email.value.trim();
   const messageValue = message.value.trim();
 
-  // Перевіряємо, чи обидва поля форми заповнені
   if (emailValue && messageValue) {
-    // Виводимо дані у консоль
     console.log({ email: emailValue, message: messageValue });
 
-    // Очищаємо локальне сховище та поля форми
     localStorage.removeItem('feedback-form-state');
     form.reset();
   } else {
